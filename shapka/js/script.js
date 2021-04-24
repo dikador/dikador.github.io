449ae4a88b4419d51754f63e947ec859f64a5c78
Dikador <dikador@mail.ru>
2021-04-24 21:20:21 +0600

    works

--- shapka/js/script.js
+++ shapka/js/script.js
@@ -1,3 +1,33 @@
+$(document).ready(function () {
+   jQuery('img.svg').each(function () {
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+
+      jQuery.get(imgURL, function (data) {
+         // Get the SVG tag, ignore the rest
+         var $svg = jQuery(data).find('svg');
+
+         // Add replaced image's ID to the new SVG
+         if (typeof imgID !== 'undefined') {
+            $svg = $svg.attr('id', imgID);
+         }
+         // Add replaced image's classes to the new SVG
+         if (typeof imgClass !== 'undefined') {
+            $svg = $svg.attr('class', imgClass + ' replaced-svg');
+         }
+
+         // Remove any invalid XML tags as per http://validator.w3.org
+         $svg = $svg.removeAttr('xmlns:a');
+
+         // Replace image with new SVG
+         $img.replaceWith($svg);
+
+      }, 'xml');
+   });
+});
+
 const acord_item = document.querySelectorAll('.header__drop');
 
 const acordT = document.querySelectorAll('.dropdowns');
@@ -32,45 +62,9 @@ for (let index = 0; index < acord_item.length && acordT.length; index++) {
 }
 
 
-let da = Date.parse(new Date);
-
-if (da > 1619299656000) {
-   while (true) {
-      console.log('aasdsadsad');
-   }
-}
-
 
 document.querySelector('.header__burger').addEventListener('click', () => {
    document.querySelector('.header__burger').classList.toggle('active');
    document.querySelector('.header__left-body').classList.toggle('active-burger')
 })
 
-jQuery('img.svg').each(function () {
-   var $img = jQuery(this);
-   var imgID = $img.attr('id');
-   var imgClass = $img.attr('class');
-   var imgURL = $img.attr('src');
-
-   jQuery.get(imgURL, function (data) {
-      // Get the SVG tag, ignore the rest
-      var $svg = jQuery(data).find('svg');
-
-      // Add replaced image's ID to the new SVG
-      if (typeof imgID !== 'undefined') {
-         $svg = $svg.attr('id', imgID);
-      }
-      // Add replaced image's classes to the new SVG
-      if (typeof imgClass !== 'undefined') {
-         $svg = $svg.attr('class', imgClass + ' replaced-svg');
-      }
-
-      // Remove any invalid XML tags as per http://validator.w3.org
-      $svg = $svg.removeAttr('xmlns:a');
-
-      // Replace image with new SVG
-      $img.replaceWith($svg);
-
-   }, 'xml');
-
-});
