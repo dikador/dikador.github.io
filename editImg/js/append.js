12da63f1c5e9c992e76cad166242e5fc2ad6405f
Dikador <dikador@mail.ru>
2021-07-07 19:56:24 +0600

    works

--- editImg/js/append.js
+++
@@ -1,9 +0,0 @@
-
-$('#download').appendTo('#downloads');
-$('#download').addClass('nav-link');
-
-$('#upload').appendTo('#load-img');
-$('#upload').addClass('nav-link');
-
-$('.sp-container').appendTo('.color');
-
